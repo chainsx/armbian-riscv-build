95cd6b7c2673e00debe82489fbbb8642657084ca
chainsx <pzhrhlx@outlook.com>
2023-03-11 13:21:01 +0800

    Update general.sh

--- lib/general.sh
+++ lib/general.sh
@@ -46,17 +46,6 @@
 # "sources" - delete output/sources
 #
 
-# run_host_command_logged is the very basic, should be used for everything, but, please use helpers above, this is very low-level.
-run_host_command_logged() {
-	raw_command="${raw_command:-"$*"}" run_host_command_logged_raw /bin/bash -e -o pipefail -c "$*"
-}
-
-chroot_custom() {
-	local target=$1
-	shift
-	raw_command="$*" raw_extra="chroot_custom" TMPDIR="" run_host_command_logged_raw chroot "${target}" /bin/bash -e -o pipefail -c "$*"
-}
-
 cleaning()
 {
 	case $1 in
